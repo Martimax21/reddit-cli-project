c9e561592442faf480daa9538c06f165e4161805
Maxime Martin <maxime.martin123@gmail.com>
2016-07-14 22:48:09 +0000

    redditex

--- reddit.js
+++ reddit.js
@@ -1,25 +1,32 @@
-var functions = require('./library/function.js');
+// Node modules
 
-
-//getHomepage(function(err, res){
-//  console.log(res);
-//})
+var functions = require('./library/function');
+var formatting = require('./library/format');
+var siteMap = require('./library/site-map-function');
 
 var request = require('request');
 var inquirer = require('inquirer');
+var clear = require('clear');
 
-// Basic feature: the homepage posts
+var imageToAscii = require("image-to-ascii");
+ 
 
-// When the user chooses the homepage option, you should display the list of posts from the getHomepage 
-// function you created previously. For each post, list at least some of the info that appears on reddit: 
-// title, url, votes, username. After the list of posts is displayed, you should display the main menu again.
+
+// array of main menu choices for user to pick from.
+
+  
+
+
+// Main function for program! :D 
+// inital prompt in reddit function asks user to choose a page from the main menu
+
 
 function reddit() {
   var menuChoices = [{
     name: 'Show homepage',
     value: 'HOMEPAGE'
   }, {
-    name: 'Show subreddit',
+    name: 'Pick a subreddit',
     value: 'SUBREDDIT'
   }, {
     name: 'List subreddits',
@@ -28,110 +35,141 @@ function reddit() {
     name: 'Quit',
     value: 'QUIT'
   }];
+  
   inquirer.prompt({
     type: 'list',
     name: 'menu',
     message: 'What do you want to do?',
     choices: menuChoices
-  }).then(function(answers) {
-    if (answers.menu === "HOMEPAGE") {
-      functions.getHomepage(function(err, res) {
-        res.forEach(function(array) {
-          console.log("Title: " + array.data.title);
-          console.log("URL: " + array.data.url);
-          console.log("Score: " + array.data.score);
-          console.log("Author: " + array.data.author);
+  }).then(function(menuChoice) {
+    
+// 4 main menu choices ('if' statement for each option):
+  //HOMEPAGE OPTION
+    if (menuChoice.menu === "HOMEPAGE") {
+      functions.getHomepage(function(err, posts) {
+        posts.map(formatting.formatPost).forEach(function(formattedPost) {
+          console.log(formattedPost + "\n");
         });
-        reddit();
+        siteMap.mainMenuOrExit(restartReddit);
       });
     }
-    else if (answers.menu === "SUBREDDIT") {
+  //SUBREDDIT OPTION
+    else if (menuChoice.menu === "SUBREDDIT") {
       inquirer.prompt({
         name: 'choose',
-        message: 'choose your SubReddit'
-      }).then(function(answers) {
-        functions.getSubreddit(answers.choose, function(err, res) {
-          res.forEach(function(array) {
-            console.log("Title: " + array.data.title);
-            console.log("URL: " + array.data.url);
-            console.log("Score: " + array.data.score);
-            console.log("Author: " + array.data.author);
-
+        message: 'Pick a subreddit to display'
+      }).then(function(subredditUserChoice) {
+        functions.getSubreddit(subredditUserChoice.choose, function(err, posts) {
+          posts.map(formatting.formatPost).forEach(function(formattedPost) {
+            console.log(formattedPost + "\n");
           });
-
+          siteMap.mainMenuOrExit(restartReddit);
         });
-        reddit();
-      })
+    
+      });
     }
-    else if (answers.menu === "SUBREDDITS") {
-      functions.getSubreddits(function(err, res) {
-        var subredditChoices = res.map(function(array) {
-          return {
-            name: array.data.title,
-            value: array
-          }
+  //LIST OF SUBREDDITS OPTION
+    else if (menuChoice.menu === "SUBREDDITS") {
+      functions.getSubreddits(function(err, subreddits) {
+        var subredditChoices = subreddits.map(formatting.formatList);
+      //BACK OPTION (in array of subreddit options)
+        subredditChoices.push({
+          name: "Go back",
+          value: "BACK"
         });
-
-
-        inquirer.prompt({
-          type: 'list',
-          name: 'subredditMenu',
-          message: 'Which subreddit do you want to pick?',
-          choices: subredditChoices
-        }).then(function(answers) {
-          var displayName = answers.subredditMenu.data.display_name;
-          var URLSubreddit = "https://www.reddit.com/r/" + displayName + ".json";
-        functions.getSubreddit(displayName, function(err, res) {
-        res.forEach(function(array) {
-            console.log("Title: " + array.data.title);
-            console.log("URL: " + array.data.url)
-                        console.log("Score: " + array.data.score);
-          console.log("Author: " + array.data.author);
+        //User chooses which subreddit from the list of options to look at
+          inquirer.prompt({
+            type: 'list',
+            name: 'subredditMenu',
+            message: 'Which subreddit do you want to pick?',
+            choices: subredditChoices
+          }).then(function(answers) {
+          //BACK OPTION (to main homepage)
+            if (answers.subredditMenu === "BACK") {
+              siteMap.mainMenuOrExit(restartReddit);
+            }
+          //If user chooses any option other than "BACK" (.: if they choose a subreddit):
+          //Each subreddit object is formatted to show title, url, score, author.
+            else {
+              functions.getSubreddit(answers.subredditMenu.data.display_name, function(err, posts) {
+                
+                var listOfPosts = posts.map(formatting.formatList);
+                // console.log(listOfPosts);
+                
+                inquirer.prompt({
+                  type: "list", 
+                  name: "subredditChoicePages",
+                  message: "What do you feel like reading?",
+                  choices: listOfPosts
+                }).then(function(answers) {
+                  clear();
+                  console.log("title: " + answers.subredditChoicePages.data.title + "\n");
+                  console.log("Username: " + answers.subredditChoicePages.data.author + "\n");
+                  
+                  if(answers.subredditChoicePages.data.url.indexOf(".jpg") > -1) {
+                    imageToAscii(answers.subredditChoicePages.data.url, (err, converted) => {
+                    console.log(err || converted);
+                    });
+                  }
+                  else if(answers.subredditChoicePages.data.url.indexOf(".jpeg") > -1) {
+                    imageToAscii(answers.subredditChoicePages.data.url, (err, converted) => {
+                    console.log(err || converted);
+                    });
+                  }
+                  else if(answers.subredditChoicePages.data.url.indexOf(".png") >1) {
+                    imageToAscii(answers.subredditChoicePages.data.url, (err, converted) => {
+                    console.log(err || converted);
+                    });
+                  }
+                  else if(answers.subredditChoicePages.data.url.indexOf(".gif") >1) {
+                    imageToAscii(answers.subredditChoicePages.data.url, (err, converted) => {
+                    console.log(err || converted);
+                    });
+                  }
+                  else {
+                    console.log("URL: " + answers.subredditChoicePages.data.url + "\n");
+                  }
+                  setTimeout(function() {
+                     siteMap.mainMenuOrExit(restartReddit);
+                   }, 3000);
+                
+                })
+                
+                
+              });
+            }
           });
-        });
-
-        });
-
-      })
+      });
     }
-    else if (answers.menu === "QUIT") {
-      console.log("Thanks for browsing! Have a really, really, really good day!")
+    //To quit whole Reddit program, QUIT option (located in the main menu):
+    else if (menuChoice.menu === "QUIT") {
+      console.log("Thanks for browsing! Have a really, really, really good day!");
       return;
     }
   });
 }
 
+//whole program called:
 reddit();
 
-// Basic feature: the homepage posts
-// When the user chooses the homepage option, you should display the list of posts from the getHomepage 
-// function you created previously. For each post, list at least some of the info that appears on reddit: 
-// title, url, votes, username. After the list of posts is displayed, you should display the main menu again.
+/* restartReddit is a callback function located in mainMenuOrExit to go back to 
+main menu options (Homepage, subreddits, list of subreddits, quit)
+*/ 
+function restartReddit(err, res) {
+  if (!err) {
+  reddit();
+  }
+}
 
-// Basic feature: subreddit posts
-// When the user chooses the subreddit posts option, you should ask him – again using inquirer – which subreddit
-// he wants to see. Then, display the list of posts in the same way as the homepage.
+// Feature: post selection and “image” display :star::star:
 
-// Feature: list of subreddits :star:
-// When the user chooses the list of subreddits option, you should load the list of subreddits using the 
-// getSubreddits function you created previously. Then, using inquirer, show the list of subreddits to the user. 
-// The user will be able to choose a subreddit to display its posts, or go back to the main menu. You can use an 
-// Inquirer Separator to create a visual separation between the list of subreddits and the “go back to main menu” 
-// option.
+// When the user is shown a list of posts, instead of going back to the main menu every
+// post should be selectable – again using inquirer. When selecting a post, the terminal screen 
+// should be cleared and only that post should be displayed (title + url + username). In addition to this,
+// if the URL of the post turns out to be an image – ends in .jpg, .gif or .png – you should use the image-to-ascii 
+// module to load the image and display it on the command line. After the post details are displayed, you should
+// show the main menu again.
 
 
-// inquirer.prompt({
-//           type: 'list',
-//           name: 'menu',
-//           message: 'What do you want to do?',
-//           choices: choices
-//         }).then(function(answers) {
-//           console.log(answers)
-//         })
 
-// res.forEach(function(array) {
-//             console.log("Title: " + array.data.title);
-//             console.log("URL: " + array.data.url);
-//             console.log("Score: " + array.data.score);
-//             console.log("Author: " + array.data.author);
-//           });
+
