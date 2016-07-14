6cf1c69dc7ad90a3296963e14cf12e307e78d19a
Maxime Martin <maxime.martin123@gmail.com>
2016-07-14 22:49:11 +0000

    formatt

---
+++ library/format.js
@@ -0,0 +1,29 @@
+
+function formatPost(post) {
+  return [
+    "Title: " + post.data.title,
+    "URL: " + post.data.url,
+    "Score: " + post.data.score,
+    "Username: "  + post.data.author
+  ].join("\n");
+}
+function formatList(item) {
+  return {
+    name: item.data.title,
+    value: item
+  };
+}
+function formatSubredditPost(post) {
+  return [
+    "Title: " + post.data.title,
+    "URL: " + post.data.url,
+    "Username: "  + post.data.author
+  ].join("\n");
+}
+
+module.exports = {
+    formatPost: formatPost,
+    formatList: formatList,
+    formatSubredditPost: formatSubredditPost
+};
+
