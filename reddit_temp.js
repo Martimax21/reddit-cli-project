cbc166ea8ed298bdfec54b97fccf8256798b42c1
Maxime Martin <maxime.martin123@gmail.com>
2016-07-14 00:30:26 +0000

    reddittemp

---
+++ reddit_temp.js
@@ -0,0 +1,21 @@
+var inquirer = require('inquirer');
+
+var menuChoices = [
+  {name: 'Show homepage', value: 'HOMEPAGE'},
+  {name: 'Show subreddit', value: 'SUBREDDIT'},
+  {name: 'List subreddits', value: 'SUBREDDITS'}
+];
+
+inquirer.prompt({
+  type: 'list',
+  name: 'menu',
+  message: 'What do you want to do?',
+  choices: menuChoices
+}).then(
+  function(answers) {
+    if (answers === "HOMEPAGE") {
+        getHomepage()
+    };
+  }
+);
+
