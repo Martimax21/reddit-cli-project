d533b6a941927e2ff9e45b88beb7403160ba4719
Maxime Martin <maxime.martin123@gmail.com>
2016-07-15 21:41:09 +0000

    reddit_temp

--- reddit_temp.js
+++ reddit_temp.js
@@ -19,3 +19,37 @@ inquirer.prompt({
   }
 );
 
+
+var commentObjects = commentsParsed[1].data.children;
+      var listOfComments = commentObjects.map(function(comment){
+        var repliesLev1 = comment.data.replies.data.children.map(function(reply){
+          var repliesLev2 = reply.data.replies.data.children.map(function (reply2){
+              return reply2.data.body;
+          })
+          return [reply.data.body, repliesLev2];
+        })
+        return repliesLev1.unshift(comment.data.body);
+      });
+          
+        var listOfReplyLev1 =commentObjects
+          //.foreach(function(commentReply){
+          //   console.log(commentReply + "\n")
+          // });
+          console.log("comments: " + comment.data.body);
+          console.log("replies:" + mappedReplies);
+//          console.log("BUGGG");
+//         return comment.data.body;
+        });
+        
+        
+        //           var mappedReplies = comment.data.replies.data.children.map(function(reply){
+//           return reply.data.body;
+//           })
+//           //.foreach(function(commentReply){
+//           //   console.log(commentReply + "\n")
+//           // });
+//           console.log("comments: " + comment.data.body);
+//           console.log("replies:" + mappedReplies);
+// //          console.log("BUGGG");
+// //         return comment.data.body;
+//         });
